bd16c49ce57dec1d0d2a1f0adc79fb098739630b
MunnaAhmed130 <munnaahmed2025@gmail.com>
2022-03-27 19:12:20 +0600

    fixed navbar

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import Header from '../../Shared/Header/Header';
 import './Banner.css';
 
 const Banner = () => {
@@ -8,41 +9,45 @@ const Banner = () => {
     const img3 = "https://i.ibb.co/5L2LNjB/image-copyright-51.jpg"
     const img4 = "https://i.ibb.co/GCKctkK/image-copyright-46.jpg"
     return (
-        <Carousel fade>
-            <Carousel.Item interval={2500}>
-                <img
-                    className="d-block w-100 banner-img"
-                    src={img1}
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3>Bicycles for Adventures</h3>
-                    <p>We create the best Cycling Experiences of a lifetime.We can fit you with the perfect bike because we carry all sizes and types of bikes.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={2500}>
-                <img
-                    className="d-block w-100 banner-img"
-                    src={img2}
-                    alt="Second slide"
-                />
-                <Carousel.Caption>
-                    <h3>Fashionable Bicycles</h3>
-                    <p>We create the best Cycling Experiences of a lifetime.We can fit you with the perfect bike because we carry all sizes and types of bikes.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={2500}>
-                <img
-                    className="d-block w-100 banner-img"
-                    src={img3}
-                    alt="Third slide"
-                />
-                <Carousel.Caption>
-                    <h3>Bicycles for Roads</h3>
-                    <p>We create the best Cycling Experiences of a lifetime.We can fit you with the perfect bike because we carry all sizes and types of bikes.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+        <div>
+
+            <Carousel fade>
+
+                <Carousel.Item interval={2500}>
+                    <img
+                        className="d-block w-100 banner-img"
+                        src={img1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption className='caption'>
+                        <h3>Bicycles for Adventures</h3>
+                        <p>We create the best Cycling Experiences of a lifetime.We can fit you with the perfect bike because we carry all sizes and types of bikes.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={2500}>
+                    <img
+                        className="d-block w-100 banner-img"
+                        src={img2}
+                        alt="Second slide"
+                    />
+                    <Carousel.Caption className='caption'>
+                        <h3>Fashionable Bicycles</h3>
+                        <p>We create the best Cycling Experiences of a lifetime.We can fit you with the perfect bike because we carry all sizes and types of bikes.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={2500}>
+                    <img
+                        className="d-block w-100 banner-img"
+                        src={img3}
+                        alt="Third slide"
+                    />
+                    <Carousel.Caption className='caption'>
+                        <h3>Bicycles for Roads</h3>
+                        <p>We create the best Cycling Experiences of a lifetime.We can fit you with the perfect bike because we carry all sizes and types of bikes.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
     );
 };
 
--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,30 +1,47 @@
 import { Button } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import { HashLink } from 'react-router-hash-link';
 import useAuth from '../../../Hooks/useAuth';
-import Footer from '../Footer/Footer';
+// import Footer from '../Footer/Footer';
 import './Header.css';
 
 const Header = () => {
+    const [navbar, setNavbar] = useState(false);
     const { user, logOut } = useAuth();
+
+    const changeBackground = () => {
+        if (window.scrollY >= 64) {
+            setNavbar(true);
+        }
+        else {
+            setNavbar(false);
+        }
+        console.log(window.scrollY)
+    };
+
+    window.addEventListener('scroll', changeBackground);
     return (
         <div className='header'>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Container>
+            <Navbar collapseOnSelect expand="lg"
+                fixed="top"
+                // variant={navbar ? 'light' : "dark"}
+                variant="dark"
+                className={navbar ? 'navbar active' : "navbar"}>
+                <Container className='my-3'>
                     <Navbar.Brand href="#home">Bicycle Store</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav" className="">
-                        <Nav className="ms-auto">
-                            <Nav.Link as={HashLink} className="" to="/home">Home</Nav.Link>
-                            <Nav.Link as={HashLink} className="" to="/explore">Explore</Nav.Link>
-                            <Nav.Link as={HashLink} className="" to={`/dashboard/dashboard`}>Dashboard</Nav.Link>
-                            {user.displayName && <Nav.Link className=''>{user.displayName}</Nav.Link>}
+                        <Nav className="ms-auto links">
+                            <Nav.Link as={HashLink} smooth className="link" to="/home">Home</Nav.Link>
+                            <Nav.Link as={HashLink} className="link" to="/explore">Explore</Nav.Link>
+                            <Nav.Link as={HashLink} className="link" to={`/dashboard/dashboard`}>Dashboard</Nav.Link>
+                            {user.displayName && <Nav.Link className='link'>{user.displayName}</Nav.Link>}
 
 
                             {user.photoURL && <img className='profile-pic' src={user.photoURL} alt="dd" />}
 
-                            {user.email ? <Button className='logOut-btn' variant='outlined' color="error" onClick={logOut}>Log out</Button> : <Nav.Link as={HashLink} className="" to="/login">Login</Nav.Link>}
+                            {user.email ? <Button className='logOut-btn' variant='outlined' color="error" onClick={logOut}>Log out</Button> : <Nav.Link as={HashLink} className="link" to="/login">Login</Nav.Link>}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
