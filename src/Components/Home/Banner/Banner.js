6750bf99e2c3854d251972d342882858da2a0522
Munna Ahmed <munnaahmed2025@gmail.com>
2023-05-12 14:06:32 +0600

    commit

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,47 +1,44 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
-// import Header from '../../Shared/Header/Header';
 import "./Banner.css";
 
 const Banner = () => {
   const img1 = "https://i.ibb.co/SrxdJ8z/background.png";
   const img2 = "https://i.ibb.co/MNtH00X/luca-david-ia8u-TRs-ZZYY-unsplash.jpg";
-  //   const img3 = "https://i.ibb.co/5L2LNjB/image-copyright-51.jpg";
-  //   const img4 = "https://i.ibb.co/GCKctkK/image-copyright-46.jpg";
+
+  const interval = 5000;
+
   return (
-    <div>
-      <Carousel fade>
-        <Carousel.Item interval={2500}>
-          <img
-            className="d-block w-100 banner-img"
-            src={img1}
-            alt="First slide"
-          />
-          <Carousel.Caption className="caption">
-            <h3>Bicycles for Adventures</h3>
-            <p>
-              We create the best Cycling Experiences of a lifetime.We can fit
-              you with the perfect bike because we carry all sizes and types of
-              bikes.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={2500}>
-          <img
-            className="d-block w-100 banner-img"
-            src={img2}
-            alt="Second slide"
-          />
-          <Carousel.Caption className="caption">
-            <h3>Fashionable Bicycles</h3>
-            <p>
-              We create the best Cycling Experiences of a lifetime.We can fit
-              you with the perfect bike because we carry all sizes and types of
-              bikes.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        {/* <Carousel.Item interval={2500}>
+    <Carousel fade>
+      <Carousel.Item interval={interval}>
+        <img
+          className="d-block w-100 object-fit-cover banner-img "
+          src={img1}
+          alt="First slide"
+        />
+        <Carousel.Caption className="caption">
+          <h3>Bicycles for Adventures</h3>
+          <p>
+            We create the best Cycling Experiences of a lifetime.We can fit you
+            with the perfect bike because we carry all sizes and types of bikes.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={interval}>
+        <img
+          className="d-block w-100 object-fit-cover banner-img"
+          src={img2}
+          alt="Second slide"
+        />
+        <Carousel.Caption className="caption">
+          <h3>Fashionable Bicycles</h3>
+          <p>
+            We create the best Cycling Experiences of a lifetime.We can fit you
+            with the perfect bike because we carry all sizes and types of bikes.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      {/* <Carousel.Item interval={2500}>
           <img
             className="d-block w-100 banner-img"
             src={img3}
@@ -56,8 +53,7 @@ const Banner = () => {
             </p>
           </Carousel.Caption>
         </Carousel.Item> */}
-      </Carousel>
-    </div>
+    </Carousel>
   );
 };
 
--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,8 +1,7 @@
 import { Button } from "@mui/material";
 import React, { useState } from "react";
-import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import useAuth from "../../../Hooks/useAuth";
-// import Footer from '../Footer/Footer';
 import "./Header.css";
 
 const Header = () => {
@@ -20,56 +19,54 @@ const Header = () => {
 
   window.addEventListener("scroll", changeBackground);
   return (
-    <div className="header">
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        fixed="top"
-        // variant={navbar ? 'light' : "dark"}
-        variant="dark"
-        className={navbar ? "navbar active" : "navbar"}
-      >
-        <Container className="my-3">
-          <Navbar.Brand href="#home">Bicycle Store</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav" className="">
-            <Nav className="ms-auto links">
-              <Nav.Link className="link" to="/home">
-                Home
-              </Nav.Link>
-              <Nav.Link className="link" to="/explore">
-                Explore
-              </Nav.Link>
-              <Nav.Link className="link" to={`/dashboard/dashboard`}>
-                Dashboard
-              </Nav.Link>
-              {user.displayName && (
-                <Nav.Link className="link">{user.displayName}</Nav.Link>
-              )}
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      // fixed="top"
+      // variant={navbar ? "light" : "dark"}
+      variant="dark"
+      className={navbar ? "navbar active" : "navbar"}
+    >
+      <Container>
+        <Navbar.Brand href="#home">Bicycle Store</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className="">
+          <Nav className="ms-auto links">
+            <Nav.Link className="link" to="/home">
+              Home
+            </Nav.Link>
+            <Nav.Link className="link" to="/explore">
+              Explore
+            </Nav.Link>
+            <Nav.Link className="link" to={`/dashboard/dashboard`}>
+              Dashboard
+            </Nav.Link>
+            {user.displayName && (
+              <Nav.Link className="link">{user.displayName}</Nav.Link>
+            )}
 
-              {user.photoURL && (
-                <img className="profile-pic" src={user.photoURL} alt="dd" />
-              )}
+            {user.photoURL && (
+              <img className="profile-pic" src={user.photoURL} alt="dd" />
+            )}
 
-              {user.email ? (
-                <Button
-                  className="logOut-btn"
-                  variant="outlined"
-                  color="error"
-                  onClick={logOut}
-                >
-                  Log out
-                </Button>
-              ) : (
-                <Nav.Link className="link" to="/login">
-                  Login
-                </Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+            {user.email ? (
+              <Button
+                className="logOut-btn"
+                variant="outlined"
+                color="error"
+                onClick={logOut}
+              >
+                Log out
+              </Button>
+            ) : (
+              <Nav.Link className="link" to="/login">
+                Login
+              </Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
